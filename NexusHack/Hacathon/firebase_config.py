bc42a25e449e0f11e30a588bbbff75ceffb972d4
allenlobo31 <allenlobo05@gmail.com>
2024-12-10 22:44:21 +0530

    Delete NexusHack/Hacathon directory

--- NexusHack/Hacathon/firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("path/to/serviceAccountKey.json")  # Your downloaded Firebase service account key
-firebase_admin.initialize_app(cred)
-
-# Initialize Firestore (optional)
-db = firestore.client()
